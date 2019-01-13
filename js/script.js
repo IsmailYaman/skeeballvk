68374214e476520cba256940c4e05eda9f3de710
IsmailYaman <iso_yaman@outlook.com>
2019-01-13 16:02:16 +0100

    rework carousel, typo edits

--- js/script.js
+++ js/script.js
@@ -6,18 +6,17 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-    // let i;
-    let slides = document.getElementsByClassName("slide");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    //met forloop laat de volgende slide zien en verwijderd de vorige slide
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // slides[slideIndex-1].style.display = "block";
+
+    slides[slideIndex-1].style.display = "block";
+
 }
-
-
 function collapse() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
